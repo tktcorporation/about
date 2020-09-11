4a082d2dc23addbde5c823d1f1fa5d14adff9d8f
tkt <tktcorporation.go@gmail.com>
2020-09-12 01:10:55 +0900

    resolve path mapping to link component

--- app/src/component/profile/links/Links.tsx
+++ app/src/component/profile/links/Links.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "../../Link";
+import { Link } from "../../link/Link";
 import "./Links.scss";
 
 export const Links = () => {
--- app/src/component/profile/products/Products.tsx
+++ app/src/component/profile/products/Products.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "../../Link";
+import { Link } from "../../link/Link";
 import "./Products.scss";
 
 export const Products = () => {
